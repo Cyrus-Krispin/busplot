f1670a428daebbf6046b19e1cd6e4724e1a941e3
Cyrus Krispin <cyruskrispin@gmail.com>
2025-09-29 19:30:49 +0800

    first commit

---
+++ src/main/java/com/busplot/busplot/BusplotApplication.java
@@ -0,0 +1,13 @@
+package com.busplot.busplot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusplotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusplotApplication.class, args);
+	}
+
+}
